25263fcb600d5053e0fe2eccbf334f1b0e53f818
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-04-04 01:24:53 -0300

    Criada a clase AppContext

---
+++ projetoTelas/projetoTelas/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoTelas
+{
+    class AppContext : DbContext
+    {
+        public AppContext() : base("name=DbApp")
+        {
+            Database.SetInitializer<AppContext>(new CreateDatabaseIfNotExists<AppContext>());
+        }
+    }
+}
